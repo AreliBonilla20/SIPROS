3a7c0cbe4034e2948751ddfa36ef17b48f97f69a
AreliBonilla20 <areli.bonilla.ramos.abr@gmail.com>
2020-07-10 16:43:25 -0600

    Plantlla

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,50 @@
 |
 */
 
+//Login
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Rutas de los expedientes
+Route::get('/expedientes_listado', function () {
+    return view('Expedientes/expedientes_listado');
+});
+
+Route::get('/expediente_nuevo', function () {
+    return view('Expedientes/expediente_nuevo');
+});
+
+
+//Rutas de las instituciones
+Route::get('/instituciones_listado', function () {
+    return view('Instituciones/instituciones_listado');
+});
+
+Route::get('/institucion_nueva', function () {
+    return view('Instituciones/institucion_nueva');
+});
+
+
+//Rutas de los proyectos
+Route::get('/proyectos_listado', function () {
+    return view('Proyectos/proyectos_listado');
+});
+
+Route::get('/proyecto_nuevo', function () {
+    return view('Proyectos/proyecto_nuevo');
+});
+
+
+//Rutas para los usuarios
+Route::get('/usuarios_listado', function () {
+    return view('Usuarios/usuarios_listado');
+});
+
+Route::get('/usuario_nuevo', function () {
+    return view('Usuarios/usuario_nuevo');
+});
