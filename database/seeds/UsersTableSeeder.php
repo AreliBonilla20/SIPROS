a97168793cca862a6bba01bdf95387b2b759fafb
arlegomez <40054974+arlegomez@users.noreply.github.com>
2020-07-17 17:55:13 -0600

    fix usuario

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Caffeinated\Shinobi\Models\Role;
+use Illuminate\Support\Facades\DB;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,12 +14,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 3)->create();
+        User::create([
+            'name'=> 'Admin', 
+            'email'=> 'admin@gmail.com',
+            'password'=> Hash::make('secret')
+        ]);
 
         Role::create([
             'name'      =>   'Admin',
             'slug'      =>   'admin',
             'special'   =>    'all-access'
+        ]);
+
+        DB::table('role_user')->insert([
+            'role_id' => 1,
+            'user_id' => 1
         ]);
     }
 }
